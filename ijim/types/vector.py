f31b1fef849bf813aecc1216fbf6d82bb088d8f5
smlu <smlu@s5.net>
2019-02-16 23:57:03 +0100

    Update vector.py

--- ijim/types/vector.py
+++ ijim/types/vector.py
@@ -1,5 +1,25 @@
-from typing import Tuple
+from typing import NamedTuple
 
-Vector2f = Tuple[float, float]
-Vector3f = Tuple[float, float, float]
-Vector4f = Tuple[float, float, float, float]
+class Vector2f(NamedTuple):
+    x: float
+    y: float
+
+    def __repr__(self) -> str:
+        return f'<Vector2f ({self.x}, {self.y})>'
+
+class Vector3f(NamedTuple):
+    x: float
+    y: float
+    z: float
+
+    def __repr__(self) -> str:
+        return f'<Vector3f ({self.x}, {self.y}, {self.z})>'
+
+class Vector4f(NamedTuple):
+    x: float
+    y: float
+    z: float
+    w: float
+
+    def __repr__(self) -> str:
+        return f'<Vector4f ({self.x}, {self.y}, {self.z}, {self.w})>'
